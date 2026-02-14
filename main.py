63356adab1678bf41539d6b6a8067e8fa55fb529
Jasvaran Mann <mannjesse355@gmail.com>
2026-02-13 23:07:45 -0800

    created handler function for received data and coolant temp PID request

--- main.py
+++ main.py
@@ -3,19 +3,57 @@ import pprint
 from bleak import BleakScanner, BleakClient
 
 async def main():
-    devices = await BleakScanner.discover(10.0, return_adv=True)
-    for i, d in enumerate(devices):
-        adv_data = devices[d][1]
-        print(f"{i}: {adv_data}")
-        address = d.address if hasattr(d, "address") else d
-        try:
-            async with BleakClient(address) as client:
-                print(f"Connected to {address}")
-                for service in client.services:
-                    print(f"  Service: {service.uuid}")
-                    for char in service.characteristics:
-                        print(f"    Characteristic: {char.uuid}, properties: {char.properties}")
-        except Exception as e:
-            print(f"Could not connect to {address}: {e}")
+    TX_UUID = None
+    RX_UUID = None
+    my_device = None
+    devices = await BleakScanner.discover(5.0, return_adv=True)
+    for d in devices:
+        if devices[d][1].local_name == 'IOS-Vlink':
+            print("Found Vlink OBD2!")
+            my_device = d
+            print(my_device)
+    
+    if not my_device:
+        print("Could not find Vlink OBD2")
+        return
+    
+    address = my_device
+    async with BleakClient(address) as client:
+        print(f"Connected to {address}")
+        for service in client.services:
+            for char in service.characteristics:
+                props = char.properties
+                if ("write" in props or "write-without-response" in props) and TX_UUID is None:
+                    TX_UUID = char.uuid
+                if ("notify" in props or "indicate" in props) and RX_UUID is None:
+                    RX_UUID = char.uuid
+                pprint.pprint(f"Characteristic: {char.uuid}, Properties: {char.properties}, Description: {char.description}")
+        print(f"TX_UUID: {TX_UUID}")
+        print(f"RX_UUID: {RX_UUID}")
+
+        # Notification handler function
+        def notificaion_handler(sender, data):
+            print(f"Notification from {sender}: {data.decode(errors='ignore')}")
+        
+        # start notifications
+        await client.start_notify(RX_UUID, notificaion_handler)
+        
+        # Send innitalization command
+        init_command = "ATZ\r"
+        await client.write_gatt_char(TX_UUID, init_command.encode())
+
+        # wait for response
+        await asyncio.sleep(3)
+
+        print("Sending OBD2 command to read coolant temperature...")
+        await client.write_gatt_char(TX_UUID, "0105\r".encode())
+        
+        await asyncio.sleep(3)
+        # stop notifications
+        await client.stop_notify(RX_UUID)
+
+        
+
+   
 
 asyncio.run(main())
