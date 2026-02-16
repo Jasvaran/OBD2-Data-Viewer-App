1467b1e269b69eaf29d5aba1151cf375e70fc915
Jasvaran Mann <mannjesse355@gmail.com>
2026-02-15 21:20:12 -0800

    Added functionality to select device via input and added simulation UUIDs to test connection via BleakClient

--- main.py
+++ main.py
@@ -3,33 +3,63 @@ import pprint
 from bleak import BleakScanner, BleakClient
 
 async def main():
+
+    simulation_on = False
+    device_dict = {}
     TX_UUID = None
     RX_UUID = None
     my_device = None
     devices = await BleakScanner.discover(5.0, return_adv=True)
-    for d in devices:
-        if devices[d][1].local_name == 'IOS-Vlink':
-            print("Found Vlink OBD2!")
-            my_device = d
-            print(my_device)
+
+    for i, d in enumerate(devices):
+        device_dict[i] = devices[d][1].local_name
+    #     if devices[d][1].local_name == 'IOS-Vlink':
+    #         print("Found Vlink OBD2!")
+    #         my_device = d
+    #         print(my_device)
+    #         break
     
-    if not my_device:
-        print("Could not find Vlink OBD2")
+    pprint.pprint(device_dict)
+    
+    select = input("Enter the number corresponding to the device you want to connect to: ")
+    try:
+        select = int(select)
+        if select in device_dict:
+            print(f"Selected device: {device_dict[select]}")
+            my_device = list(devices.keys())[select]
+        else:
+            print("Invalid selection. Exiting.")
+            return
+    except ValueError:
+        print("Invalid input. Please enter a number. Exiting.")
         return
+    # if not my_device:
+    #     print("Could not find Vlink OBD2")
+    #     return
     
     address = my_device
+    print(address)
+
+
     async with BleakClient(address) as client:
         print(f"Connected to {address}")
-        for service in client.services:
-            for char in service.characteristics:
-                props = char.properties
-                if ("write" in props or "write-without-response" in props) and TX_UUID is None:
-                    TX_UUID = char.uuid
-                if ("notify" in props or "indicate" in props) and RX_UUID is None:
-                    RX_UUID = char.uuid
-                pprint.pprint(f"Characteristic: {char.uuid}, Properties: {char.properties}, Description: {char.description}")
-        print(f"TX_UUID: {TX_UUID}")
-        print(f"RX_UUID: {RX_UUID}")
+
+        if simulation_on == True:
+            TX_UUID = "12345678-1234-1234-1234-1234567890ac"
+            RX_UUID = "12345678-1234-1234-1234-1234567890ad"
+            print(f"TX_UUID: {TX_UUID}")
+            print(f"RX_UUID: {RX_UUID}")
+        else:
+            for service in client.services:
+                for char in service.characteristics:
+                    props = char.properties
+                    if ("write" in props or "write-without-response" in props) and TX_UUID is None:
+                        TX_UUID = char.uuid
+                    if ("notify" in props or "indicate" in props) and RX_UUID is None:
+                        RX_UUID = char.uuid
+                    pprint.pprint(f"Characteristic: {char.uuid}, Properties: {char.properties}, Description: {char.description}")
+            print(f"TX_UUID: {TX_UUID}")
+            print(f"RX_UUID: {RX_UUID}")
 
         # Notification handler function
         def notificaion_handler(sender, data):
